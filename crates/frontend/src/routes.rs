8fee734c2b36f52471af7e1a0fd37d6b9be8dc48
Baptiste Roseau <broseau@gmail.com>
2024-07-12 11:44:39 +0200

    Add a fallback to home

--- crates/frontend/src/routes.rs
+++ crates/frontend/src/routes.rs
@@ -1,10 +1,11 @@
-use crate::pages::{Home, About, Leaderboard};
+use crate::pages::{About, Home, Leaderboard};
 
 use dioxus::prelude::*;
 
 #[derive(Clone, Routable, Debug, PartialEq)]
 pub enum Routes {
     #[route("/")]
+    #[redirect("/:.._segments", |_segments: Vec<String>| Routes::Home {})]
     Home {},
     #[route("/leaderboard")]
     Leaderboard {},
