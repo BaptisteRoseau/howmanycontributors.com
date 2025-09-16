a7c72ec37816b759580b6087ca1381c1984a3e2c
Baptiste Roseau <broseau@gmail.com>
2025-09-16 19:47:37 +0200

    Factorize GIT_ROOT and remove unused SCRIPT_DIR

--- scripts/git_hooks/setup.sh
+++ scripts/git_hooks/setup.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 set -e
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+GIT_ROOT="$(git rev-parse --show-toplevel)"
 
 # Using a symbolic link to allow the script to be seamlessly updated without
 # manual intervention.
 # Using a relative path to allow the user to copy the entire repo directory
 # without affecting the git hooks execution.
-rm -f "$(git rev-parse --show-toplevel)/.git/hooks/pre-commit"
-ln -s "../../scripts/git_hooks/pre-commit" "$(git rev-parse --show-toplevel)/.git/hooks/pre-commit"
+rm -f "$GIT_ROOT/.git/hooks/pre-commit"
+ln -s "../../scripts/git_hooks/pre-commit" "$GIT_ROOT/.git/hooks/pre-commit"
 
 echo "Git hooks successfully set up"
