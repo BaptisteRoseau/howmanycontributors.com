bddc3a6e8a02614968b4fde25c0c8d73afcb2e8a
Baptiste Roseau <broseau@gmail.com>
2025-09-16 20:10:04 +0200

    Preinstall Grafana plugins and add resources to preinstall data sources and dashboards later on

--- manifests/grafana/init.sh
+++ manifests/grafana/init.sh
@@ -3,6 +3,9 @@
 #TODO: Use CLI to install plugins, create users, dashboards and alerts
 # https://grafana.com/docs/grafana/latest/cli/
 
+# For data sources and dashboards, see:
+# https://grafana.com/tutorials/provision-dashboards-and-data-sources/
+
 PLUGINS=(
     "redis-app"
     "redis-datasource"
